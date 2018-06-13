df28da45e5fdc81a67f2dd7fb4645c9caad1b3c9
kaibo_song <471908263@qq.com>
2018-06-13 21:48:52 +0800

    a first class

---
+++ class_1.py
@@ -0,0 +1,12 @@
+#定义一个学生类
+class Student():
+    name=None
+    age=18
+    course="python"
+
+    def doHomework(self):
+        print("我在做作业")
+        return None
+jingjing=Student()
+jingjing.age
+jingjing.doHomework()
